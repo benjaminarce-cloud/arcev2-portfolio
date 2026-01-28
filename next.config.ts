a79f04ce7799d394867c94299d90707153a88b66
benjaminarce-cloud <arceb3013@gmail.com>
2026-01-28 13:49:49 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [require("remark-gfm")],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX(nextConfig);
